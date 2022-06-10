4dc1a06823c29f07d4b139f7f3f54bd957093a9e
shresh <shreshthmehta1@gmail.com>
2022-06-10 15:19:12 +1000

    classes

--- doc.py
+++ doc.py
@@ -365,7 +365,6 @@ for z in sorted(basket):
 
 #syntax error 
 #while True print('Hello world')
-
 while True:
      try:
          x = int(input("Please enter a number: "))
@@ -391,3 +390,61 @@ for cls in [B, C, D]:
         print("C")
     except B:
         print("B")
+
+# Scopes and Namespaces Example
+def scope_test():
+    def do_local():
+        spam = "local spam"
+
+    def do_nonlocal():
+        nonlocal spam
+        spam = "nonlocal spam"
+
+    def do_global():
+        global spam
+        spam = "global spam"
+
+    spam = "test spam"
+    do_local()
+    print("After local assignment:", spam)
+    do_nonlocal()
+    print("After nonlocal assignment:", spam)
+    do_global()
+    print("After global assignment:", spam)
+
+scope_test()
+print("In global scope:", spam)
+
+class Complex:
+     def __init__(self, realpart, imagpart):
+         self.r = realpart
+         self.i = imagpart
+
+x = Complex(3.0, -4.5)
+print(x.r, x.i)
+
+# instance objects
+x.counter = 1
+while x.counter < 10:
+    x.counter = x.counter * 2
+print(x.counter)
+del x.counter
+
+class Dog:
+
+    def __init__(self, name):
+        self.name = name
+        self.tricks = []    # creates a new empty list for each dog
+
+    def add_trick(self, trick):
+        self.tricks.append(trick)
+
+d = Dog('Fido')
+e = Dog('Buddy')
+d.add_trick('roll over')
+e.add_trick('play dead')
+print(d.tricks)
+print(e.tricks)
+
+# inheritance
+
